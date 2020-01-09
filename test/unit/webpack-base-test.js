3a41cefbede8f97b560e2f8350a992074bcdd8c4
susan-su-0420 <sujiehua@dcrays.cn>
2020-01-09 20:09:21 +0800

    test: webpack.base test3

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,6 +5,6 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index.indexOf('/wtdc-webpack/test/smoke/template/src/index/index.js') >-1,  true);
-    assert.equal(baseConfig.entry.search.indexOf('/wtdc-webpack/test/smoke/template/src/search/index.js') >-1,  true);
+    assert.equal(baseConfig.entry.index.indexOf('wtdc-webpack/test/smoke/template/src/index/index.js') > -1,  true);
+    assert.equal(baseConfig.entry.search.indexOf('wtdc-webpack/test/smoke/template/src/search/index.js') > -1,  true);
 });
